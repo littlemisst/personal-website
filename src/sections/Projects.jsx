783112c30410e8983d5cab9edbd90b56b98d91c7
Teofy <teofyrabanes@gmail.com>
2023-06-07 21:08:14 +0800

    added content for projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -15,8 +15,13 @@ import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import Page from '../components/Page'
 import grid from "../assets/projects-grid.png"
 
+import data from "../data/data.json"
+
 function Projects() {
 
+  const projects = data.projects
+
+  console.log()
   const CardContent = ({ heading, techStack, roles, description, githubLink, liveSite }) => {
     return (
       <div className='flex content-center flex-col p-8'>
@@ -26,15 +31,15 @@ function Projects() {
         <div className='mb-3'>
           {techStack.map((tech, index) => {
             return (
-              <span key={index} className='text-xs font-nunito font-light text-light-purple bg-white rounded-full py-1 px-2 mr-2'>{tech}</span>
+              <span key={index} className='text-xs font-nunito font-light text-light-purple bg-white rounded-full py-1 px-2 mr-1'>{tech}</span>
             )
           })}
         </div>
-        <div className='mb-3'>
-          <span className='text-sm font-nunito font-bold text-white mr-2'>My Role:</span>
+        <div className='grid grid-flow-col auto-cols-max mb-3'>
+          <div className='text-sm font-nunito font-bold text-white mr-2'>My Role:</div>
           {roles.map((role, index) => {
             return (
-              <span key={index} className='text-xs font-nunito font-light text-white border border-white rounded-full py-1 px-2 mr-2'>{role}</span>
+              <div key={index} className='text-xs font-nunito font-light text-white border border-white rounded-full py-1 px-2 mr-1'>{role}</div>
             )
           })}
         </div>
@@ -42,17 +47,20 @@ function Projects() {
           <p className='text-xs font-nunito text-white leading-tight'>{description}</p>
         </div>
         <div className='flex justify-end'>
-          <button className="mr-4" onClick={() => {
+          {githubLink !== "" && <button className="mr-4" onClick={() => {
             window.open(githubLink, '_blank')
           }}>
             <FontAwesomeIcon icon={faGithub} style={{ color: "#ffffff" }} />
-          </button>
-          <button onClick={() => {
-            window.open(liveSite, '_blank')
-          }}>
+          </button>}
 
-            <FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{ color: "#ffffff" }} />
-          </button>
+          {liveSite !== "" &&
+            <button onClick={() => {
+              window.open(liveSite, '_blank')
+            }}>
+
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{ color: "#ffffff" }} />
+            </button>
+          }
         </div>
 
       </div>
@@ -75,22 +83,26 @@ function Projects() {
               modules={[EffectCards]}
               className="mySwiper"
             >
-              <SwiperSlide><CardContent heading="Slide 1" techStack={['html', 'css']} roles={["UI/UX", "Developer"]} description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet orci imperdiet, elementum dui nec, ullamcorper magna. Vestibulum eu aliquet diam. Etiam sem ante, dapibus a neque nec, egestas luctus mi. Fusce vitae consectetur urna, non mattis elit. Nullam aliquam erat eget orci molestie convallis. Ut convallis pellentesque leo, a sodales arcu maximus in." /></SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-              <SwiperSlide>Slide 3</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 5</SwiperSlide>
-              <SwiperSlide>Slide 6</SwiperSlide>
-              <SwiperSlide>Slide 7</SwiperSlide>
-              <SwiperSlide>Slide 8</SwiperSlide>
-              <SwiperSlide>Slide 9</SwiperSlide>
+              {projects.map((project) => {
+                return (
+                  <SwiperSlide key={project.id}>
+                    <CardContent
+                      heading={project.title}
+                      techStack={project.techStack}
+                      roles={project.roles}
+                      description={project.description}
+                      githubLink={project.githubLink}
+                      liveSite={project.liveLink}
+                    />
+                  </SwiperSlide>)
+              })}
             </Swiper>
           </div>
           <img
-              className='w-full h-full object-cover absolute'
-              src={grid}
-              alt="grid-bg"
-            />
+            className='w-full h-full object-cover absolute'
+            src={grid}
+            alt="grid-bg"
+          />
         </div>
       }
       id='projects'
